934933aca21506909cf205f749f91cb3a9d5876b
Ewerton Eiti Yamada <ewerton_yamada@hotmail.com>
2025-05-27 22:41:19 -0300

    first commit

---
+++ src/main/java/com/faca_receita/FacaReceitaApplication.java
@@ -0,0 +1,13 @@
+package com.faca_receita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacaReceitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacaReceitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faca_receita/FacaReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faca_receita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacaReceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
